137530d6cbfd1c21d53238c53f359c97e528a824
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2023-10-29 21:21:03 -0300

    Moving pieces (Movendo as peças)

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
@@ -26,27 +30,39 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+		
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e){
+			throw new InputMismatchException("Erro lendo Posição de Xadrez! Valores válidos são de a1 até h8.");
+		}
+	}
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print(ANSI_YELLOW + (8 - i) + " ");
+			System.out.print(ANSI_BLUE + (8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println(ANSI_YELLOW + ("  a b c d e f g h"));
+		System.out.println(ANSI_BLUE + ("  a b c d e f g h"));
 	}
 	
 	private static void printPiece(ChessPiece piece) {
     	if (piece == null) {
-            System.out.print("-");
+            System.out.print(ANSI_BLACK + ("-"));
         }
         else {
             if (piece.getColor() == Color.WHITE) {
-                System.out.print(ANSI_WHITE + piece + ANSI_YELLOW);
+                System.out.print(ANSI_WHITE + piece + ANSI_BLACK);
             }
             else {
-                System.out.print(ANSI_RED + piece + ANSI_YELLOW);
+                System.out.print(ANSI_BLACK + piece + ANSI_BLACK);
             }
         }
         System.out.print(" ");
